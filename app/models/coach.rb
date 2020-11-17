5aa1ef6cc97f74cf22fd220b2db6bde55cfad801
ebeagusamuel <samuelebeagu@gmail.com>
2020-11-17 16:22:30 +0100

    add validation for coach model attributes

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,4 +1,6 @@
 class Coach < ApplicationRecord
   has_many :appointments
   has_many :users, through: :appointments
+
+  validates_presence_of :name, :location, :qualification, :charge_per_hour
 end
