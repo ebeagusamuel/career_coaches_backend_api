8f49e2f0af44c43d202ec4d8f0006ee677c53eee
ebeagusamuel <samuelebeagu@gmail.com>
2020-11-17 16:17:10 +0100

    add validation for user model attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 
   has_many :appointments
   has_many :coaches, through: :appointments
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password_digest, presence: true
 end
