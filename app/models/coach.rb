1e4615daa08cacb0eaf02bce080f3ef77468a39d
ebeagusamuel <samuelebeagu@gmail.com>
2020-11-18 11:15:41 +0100

    add active storage to handle storing coach images an establishing an association between them

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,6 +1,7 @@
 class Coach < ApplicationRecord
   has_many :appointments
   has_many :users, through: :appointments
+  has_one_attached :image
 
   validates_presence_of :name, :location, :qualification, :charge_per_hour
 end
