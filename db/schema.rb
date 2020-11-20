c7f79628da3f394c73a4e274a366504eddb675f0
ebeagusamuel <samuelebeagu@gmail.com>
2020-11-20 21:35:33 +0100

    add location to the appointments parameters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_160206) do
+ActiveRecord::Schema.define(version: 2020_11_20_203203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_11_19_160206) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "date_and_time"
+    t.string "location"
     t.index ["coach_id"], name: "index_appointments_on_coach_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
