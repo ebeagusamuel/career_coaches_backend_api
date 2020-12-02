3c2aceaa484eac0fd6c2f9af29bfbb84731a60c9
ebeagusamuel <samuelebeagu@gmail.com>
2020-12-02 03:23:19 +0100

    add test for the coaches model

---
+++ spec/models/coach_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Coach, type: :model do
+  subject { described_class.new(name: 'John Doe',
+     location: 'My address', qualification: 'qualification',
+      charge_per_hour: '$50', phone_number: 'phone number') }
+
+  it 'is valid when all the required attributes is present' do
+    expect(subject).to be_valid
+  end
+
+  it 'is invalid when any of the required attributes is absent' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  describe 'Associations' do
+    it { should have_many(:appointments) }
+    it { should have_many(:users) }
+  end
+end
