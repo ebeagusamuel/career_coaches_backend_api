fd3534fb17e126be405c0defa59fa9a44bdefd1f
ebeagusamuel <samuelebeagu@gmail.com>
2020-12-03 12:05:05 +0100

    move transactional costly methods from the controller to the models

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -7,8 +7,7 @@ class Coach < ApplicationRecord
   validates_presence_of :name, :location, :qualification, :charge_per_hour, :phone_number
 
   def add_image_path
-    image = self.image 
-    path = rails_blob_path(image)
-    self.update_attributes(image_path: image_path)
+    path = Rails.application.routes.url_helpers.rails_blob_path(self.image, only_path: true)
+    update_columns(image_path: path)
   end
 end
